71a3e4707126bc918da94b5f6e111ceab1733f2e
Andrés Villarroel Romero <avillarroelrom@gmail.com>
2024-03-17 16:06:24 -0300

    finalCommit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,18 @@ import homeImage from '../img/home.png';
 import noteImage from '../img/note.png';
 import cakesliceImage from '../img/cakeslice.png';
 
-
 const Navbar = () => {
     return (
         <header>
             <ul>
-                    <li>
-                        <img src={homeImage} alt="" />
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <img src={noteImage} alt="" />
-                        <Link to="/contacto">Contacto</Link>
-                    </li>
+                <li>
+                    <img src={homeImage} alt="" />
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <img src={noteImage} alt="" />
+                    <Link to="/contacto">Contacto</Link>
+                </li>
                 </ul>
                 <span>Happy Cake
                     <img src={cakesliceImage} alt="" />
